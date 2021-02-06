09bd0be537ccd8aece7f572861bcb63142082145
masaun <masanoriuno@gmail.com>
2021-02-06 22:45:39 +0900

    Define Referral.sol

---
+++ contracts/PayrollMining.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract PayrollMining is Ownable {
+    
+    constructor() public {}
+
+}
--- contracts/Referral.sol
+++ contracts/Referral.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+/**
+ * @notice - 5M $WORK Rewards are issued per “Payroll Mining Block” to Employee Members, Coalition Members and Stakers. 
+ * @notice - Blocks are mined as the Network grows by increments of $50K or 5% in periodic payroll volume, whichever is greater.
+ */
 contract Referral is Ownable {
     
     constructor() public {}
