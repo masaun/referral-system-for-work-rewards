354c4c5d78a21889218feda264c068ad15516573
masaun <masanoriuno@gmail.com>
2021-02-06 11:02:07 +0900

    Add initial mint and Define mintTo()

--- contracts/WorkToken.sol
+++ contracts/WorkToken.sol
@@ -1,9 +1,22 @@
 pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WorkToken is ERC20 {
+
+contract WorkToken is ERC20, Ownable {
     
-    constructor() public ERC20("Work Token", "WORK") {}    
+    constructor() public ERC20("Work Token", "WORK") {
+        uint initialSupply = 1e8 * 1e18;           /// Initial Supply amount is 100M
+        address initialTokenHolder = msg.sender;   /// Deployer address
+        _mint(initialTokenHolder, initialSupply);  /// Deployer has 100M $WORK at first
+    }
+
+    /**
+     * @notice - Mint $WORK tokens. (Only owner address can mint)
+     */
+    function mintTo(address to, uint mintAmount) public onlyOwner returns (bool) {
+        _mint(to, mintAmount);
+    }
 
 }
