a6b4ae8694c2c8aeb8aa5209ff2b77973e780335
masaun <masanoriuno@gmail.com>
2021-02-07 09:15:22 +0900

    Define workPerBlock and startBlock in the PayrollMining.sol

--- contracts/PayrollMining.sol
+++ contracts/PayrollMining.sol
@@ -8,8 +8,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @notice - Blocks are mined as the Network grows by increments of $50K or 5% in periodic payroll volume, whichever is greater.
  */
 contract PayrollMining is Ownable {
+
+    uint256 public workPerBlock;        /// $WORK tokens created per block.
+    uint256 public startBlock;          /// The block number at which $WORK distribution starts.
+    uint256 public endBlock;            /// The block number at which $WORK distribution ends.
+    uint256 public totalAllocPoint = 0; /// Total allocation poitns. Must be the sum of all allocation points in all pools.
     
-    constructor() public {}
+    constructor() public {
+        workPerBlock = 5000000;         /// 5M $WORK Rewards are issued per “Payroll Mining Block”
+        startBlock = block.number;
+    }
 
 
+    
 }
