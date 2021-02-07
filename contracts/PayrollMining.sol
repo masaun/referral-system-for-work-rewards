1c17eb6bed0184395cd29f4074dccee315e56179
masaun <masanoriuno@gmail.com>
2021-02-07 09:50:46 +0900

    Define 'lastRewardBlock' (as the last block number that  distribution occured)

--- contracts/PayrollMining.sol
+++ contracts/PayrollMining.sol
@@ -11,9 +11,19 @@ import { WorkRewardToken } from "./WorkRewardToken.sol";
  */
 contract PayrollMining is Ownable {
 
+    // struct PoolInfo {
+    //     IERC20 lpToken;                 // Address of LP token contract.
+    //     uint256 allocPoint;             // How many allocation points assigned to this pool.
+    //     uint256 lastRewardBlock;        // Last block number that $WORK distribution occured.
+    //     uint256 accWorkPerShare;        // Accumulated $WORK per share, times 1e12. See below.
+    // }
+    // PoolInfo[] public poolInfo;         // Info of each pool.
+
+
     uint256 public workPerBlock;        /// $WORK tokens created per block.
     uint256 public startBlock;          /// The block number at which $WORK distribution starts.
     uint256 public endBlock;            /// The block number at which $WORK distribution ends.
+    uint256 public lastRewardBlock;     /// Last block number that $WORK distribution occured.
     uint256 public totalAllocPoint = 0; /// Total allocation poitns. Must be the sum of all allocation points in all pools.
 
     uint256 public lastPeriodicPayrollVolume;  /// Periodic payroll volume in the last block
