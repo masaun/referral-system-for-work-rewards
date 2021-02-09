ba9df4231c4bceea0468c30eeb12416f6e4dd715
masaun <masanoriuno@gmail.com>
2021-02-09 09:45:46 +0900

    Resume Referral.sol

---
+++ contracts/Referral.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import { MemberRegistry } from "./MemberRegistry.sol";
+
+
+/**
+ * @notice - Members receive 100% of the referral credit for Employee Members who join
+ * @notice - Members receive 15% of the referral credit for referring Coalition Organizations who act as channel partners for referring new Employee Members.
+ * @notice - NO multi-level referral credit except for referring Organizations where the referring Member receives 15%. 
+ */
+contract Referral is Ownable {
+
+    /**
+     * Entire Process for referral action
+     * ① Deploy a Linkdrop Contract
+     * ② Generate claim links
+     * ③ Send links to friends
+     * ④ One of Alice’s friends (Bob) can follow the link and claim the tokens
+     */
+
+    enum ReferralCreditType { EmployeeMember, CoalitionOrganization }
+
+    uint referralCreditRatioForEmployeeMember = 100 * 1e18;        /// 100%
+    uint referralCreditRatioForCoalitionOrganization = 15 * 1e18;  /// 15%
+
+    MemberRegistry public memberRegistry;
+
+    constructor(MemberRegistry _memberRegistry) public {
+        memberRegistry = _memberRegistry;
+    }
+
+    function grantReferralCredit(address memberAddress, ReferralCreditType referralCreditType) public returns (bool) {
+        uint referralCreditRatio;
+        if (referralCreditType == ReferralCreditType.EmployeeMember) {
+            referralCreditRatio = referralCreditRatioForEmployeeMember;         /// 100%
+        } else if (referralCreditType == ReferralCreditType.CoalitionOrganization) {
+            referralCreditRatio = referralCreditRatioForCoalitionOrganization;  /// 15%
+        }
+    }
+
+
+}
